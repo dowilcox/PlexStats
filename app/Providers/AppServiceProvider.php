c272c9e73cc8ba2ce0c8b22f7b2b915e61a896d4
Donald Wilcox <donwil92@me.com>
2014-12-22 00:33:37 -0500

    Removing things not needed.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,11 +24,6 @@ class AppServiceProvider extends ServiceProvider {
 		// This service provider is a great spot to register your various container
 		// bindings with the application. As you can see, we are registering our
 		// "Registrar" implementation here. You can add your own bindings too!
-
-		$this->app->bind(
-			'Illuminate\Contracts\Auth\Registrar',
-			'PlexStats\Services\Registrar'
-		);
 	}
 
 }
